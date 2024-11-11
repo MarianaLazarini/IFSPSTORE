0b5eeaa85801db2106c0f25f69017e9f627bed8a
Mariana Lazarini <marisolazarini@gmail.com>
2024-11-10 22:00:07 -0300

    Add files via upload

---
+++ IFSPStore.Service/Validators/CidadeValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using IFSPStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFSPStore.Service.Validators
+{
+    public class CidadeValidator : AbstractValidator<Cidade>
+    {
+        public CidadeValidator() 
+        {
+            RuleFor(c => c.Nome)
+                .NotEmpty().WithMessage("Por favor informe o nome.")
+                .NotNull().WithMessage("Por favor infome o nome.")
+                .Length(50)
+                .WithMessage("Nome pode ter no máximo 50 caractéres.");
+
+            RuleFor(c => c.Estado)
+                .NotEmpty().WithMessage("Por favor informe o Estado.")
+                .NotNull().WithMessage("Por favor infome o Estado.")
+                .Length(2);
+        }
+    }
+}
