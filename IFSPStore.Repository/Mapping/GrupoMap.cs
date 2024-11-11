8457be0b623cf6aed5929dc0b59d350822b12fd1
Mariana Lazarini <marisolazarini@gmail.com>
2024-11-10 21:58:14 -0300

    Add files via upload

---
+++ IFSPStore.Repository/Mapping/GrupoMap.cs
@@ -0,0 +1,28 @@
+﻿using IFSPStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFSPStore.Repository.Mapping
+{
+    public class GrupoMap : IEntityTypeConfiguration<Grupo>
+    {
+        public void Configure(EntityTypeBuilder<Grupo> builder)
+        {
+            //define o nome da tabela
+            builder.ToTable("Abobrinha");
+            //define a chave primária
+            builder.HasKey(x => x.Id);
+
+            builder.Property(prop => prop.Nome)
+                .HasColumnName("Melancia")
+                .IsRequired()
+                .HasMaxLength(50);
+            //.HasColumnType("varchar(50)");
+        }
+    }
+}
---
+++ IFSPStore.Repository/Mapping/UsuarioMap.cs
@@ -0,0 +1,38 @@
+﻿using IFSPStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFSPStore.Repository.Mapping
+{
+    public class UsuarioMap : IEntityTypeConfiguration<Usuario>
+    {
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            builder.ToTable("Usuario");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Nome)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            builder.Property(x => x.Senha)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            builder.Property(x => x.Email)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            builder.Property(x => x.Login)
+                .IsRequired()
+                .HasMaxLength(255);
+
+        }
+    }
+}
